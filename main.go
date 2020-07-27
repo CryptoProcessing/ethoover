66ad4de195633e70acbd6ff33cf6bab46acc010e
Evgeny Khashin <adastreamer@gmail.com>
2020-07-27 18:30:04 +0300

    enode as argument

--- main.go
+++ main.go
@@ -12,6 +12,6 @@ import (
 )
 
 func main() {
-	var node *enode.Node = utils.NewNode()
+	var node *enode.Node = utils.NewNode("enode://8b4b5f437edebb1ec85eabdb3fd966576ea60710b83cb1e71d698369e92837bd76f5b557736230651f10e928d9e66e39388405f3d57edfa3e4aa4a083c18210e@127.0.0.1:30303")
 	fmt.Println(node.IP())
 }
--- utils/enode.go
+++ utils/enode.go
@@ -20,6 +20,6 @@ func UintID(i uint16) enode.ID {
 }
 
 // returns new node
-func NewNode() *enode.Node {
-	return enode.MustParse("enode://8b4b5f437edebb1ec85eabdb3fd966576ea60710b83cb1e71d698369e92837bd76f5b557736230651f10e928d9e66e39388405f3d57edfa3e4aa4a083c18210e@40.91.195.155:30303")
+func NewNode(uri string) *enode.Node {
+	return enode.MustParse(uri)
 }
